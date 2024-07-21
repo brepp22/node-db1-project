07cbf79280fd9928686f9545e3779bb2ca63d0a7
brepp22 <brepp@hotmail.com>
2024-07-20 23:03:29 -0700

    project updates

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,68 @@
-exports.checkAccountPayload = (req, res, next) => {
+const db = require('./accounts-model')
+
+function checkAccountPayload (req, res, next) {
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
+  const error = {status: 400}
+  const { name , budget } = req.body
+  if(name === undefined || budget === undefined){
+    error.message = "name and budget are required"
+    next(error)
+  } else if (typeof budget !== 'number') {
+    error.message = "budget of account must be a number"
+    next(error)
+  } else if ( name.trim().length < 3 || name.trim().length > 100){
+    error.message = "name of account must be between 3 and 100"
+    next(error)
+  } else if (budget < 1 || budget > 100000000){
+    error.message = "budget of account is too large or too small" 
+    next(error)
+  }
+  if(error.message){
+    next(error)
+  } else{
+    next()
+  }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
+ async function checkAccountNameUnique(req, res, next) {
   // DO YOUR MAGIC
+  try{
+    const exisiting = await db('accounts')
+    .where('name' , req.body.name.trim())
+    .first()
+
+    if(exisiting){
+      next({status: 400 , message: "that name is taken" })
+    } else{
+      next()
+    }
+  }
+  catch (err) {
+    next(err)
+  }
 }
 
-exports.checkAccountId = (req, res, next) => {
+async function checkAccountId(req, res, next) {
   // DO YOUR MAGIC
+  try {
+    const accounts = await db.getById(req.params.id)
+    if(accounts){
+      req.account = accounts
+      next ()
+    } else {
+      next({ status : 404 , message: "account not found" })
+    }
+
+  } catch (err) {
+    next(err)
+  }
 }
+
+module.exports = {
+  checkAccountId,
+  checkAccountNameUnique,
+  checkAccountPayload,
+}
+
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,11 +1,27 @@
 const router = require('express').Router()
+const Accounts = require('./accounts-model')
+const { checkAccountId , checkAccountNameUnique , checkAccountPayload } = require('./accounts-middleware')
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+    const accounts = await Accounts.getAll()
+    res.json(accounts)
+  }
+  catch(err){
+    next(err)
+  }
+  })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', checkAccountId , async (req, res, next) => {
   // DO YOUR MAGIC
+ try{
+  const account = await Accounts.getById(req.params.id)
+  res.json(account)
+ }
+  catch (err){
+    next(err)
+  }
 })
 
 router.post('/', (req, res, next) => {
@@ -22,6 +38,10 @@ router.delete('/:id', (req, res, next) => {
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message , 
+    stack: err.stack,
+  })
 })
 
 module.exports = router;
